78935fba0923951d10aefc966376c2dbecd224c4
dzkdzk <dzdzaakk@gmail.com>
2025-05-01 21:47:52 +0700

    implement new method get by email

--- internal/user/repository/user_repository.go
+++ internal/user/repository/user_repository.go
@@ -7,6 +7,7 @@ import (
 type UserRepository interface {
 	Create(user model.TUser) error
 	GetByID(id int) (*model.TUser, error)
+	GetByEmail(email string) (*model.TUser, error)
 	Update(user model.TUser) error
 	Delete(id int) error
 	GetAll() ([]*model.TUser, error)
--- internal/user/repository/user_repository_impl.go
+++ internal/user/repository/user_repository_impl.go
@@ -61,6 +61,21 @@ func (r *userRepositoryImpl) GetByID(id int) (*model.TUser, error) {
 	return &user, nil
 }
 
+func (r *userRepositoryImpl) GetByEmail(email string) (*model.TUser, error) {
+	query := `SELECT id, name, email, password, role_id, role_name, last_access FROM users WHERE email= $1`
+	row := r.db.QueryRow(query, email)
+
+	var user model.TUser
+	err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.RoleID, &user.RoleName, &user.LastAccess)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
 func (r *userRepositoryImpl) Update(user model.TUser) error {
 	query := `UPDATE users SET name = $1, email = $2, password = $3, role_id = $4, role_name = $5, last_access = $6
 			  WHERE id = $7`
