705b22356f546e4678a3824384126813e4934bba
dzkdzk <dzdzaakk@gmail.com>
2025-05-01 22:36:45 +0700

    implement user usecase

--- internal/user/usecase/user_usecase_impl.go
+++ internal/user/usecase/user_usecase_impl.go
@@ -1,6 +1,11 @@
 package usecase
 
 import (
+	"database/sql"
+	"fmt"
+	"strconv"
+	"time"
+
 	"github.com/dzaakk/simple-grpc-app/internal/user/model"
 	"github.com/dzaakk/simple-grpc-app/internal/user/repository"
 )
@@ -15,37 +20,111 @@ func NewUserUseCase(repo repository.UserRepository) UserUseCase {
 	}
 }
 
-// CreateUser implements UserUseCase.
 func (u *userUseCaseImpl) CreateUser(user model.User) error {
-	panic("unimplemented")
+	dbUser := model.TUser{
+		Name:       user.Name,
+		Email:      user.Email,
+		Password:   user.Password,
+		RoleID:     user.RoleID,
+		RoleName:   user.RoleName,
+		LastAccess: sql.NullTime{Time: time.Now(), Valid: true},
+	}
+	return u.repo.Create(dbUser)
 }
 
-// DeleteUser implements UserUseCase.
 func (u *userUseCaseImpl) DeleteUser(id int) error {
-	panic("unimplemented")
+	return u.repo.Delete(id)
 }
 
-// GetAllUsers implements UserUseCase.
 func (u *userUseCaseImpl) GetAllUsers() ([]*model.User, error) {
-	panic("unimplemented")
+	listUser, err := u.repo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var users []*model.User
+	for _, user := range listUser {
+		users = append(users, &model.User{
+			Name:       user.Name,
+			Email:      user.Email,
+			RoleID:     user.RoleID,
+			RoleName:   user.RoleName,
+			LastAccess: user.LastAccess.Time.String(),
+		})
+	}
+
+	return users, nil
 }
 
-// GetUserByEmail implements UserUseCase.
 func (u *userUseCaseImpl) GetUserByEmail(email string) (*model.User, error) {
-	panic("unimplemented")
+	dbUser, err := u.repo.GetByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	if dbUser == nil {
+		return nil, nil
+	}
+
+	return &model.User{
+		Name:       dbUser.Name,
+		Email:      dbUser.Email,
+		RoleID:     dbUser.RoleID,
+		RoleName:   dbUser.RoleName,
+		LastAccess: dbUser.LastAccess.Time.String(),
+	}, nil
 }
 
 // GetUserByID implements UserUseCase.
 func (u *userUseCaseImpl) GetUserByID(id int) (*model.User, error) {
-	panic("unimplemented")
+	dbUser, err := u.repo.GetByID(id)
+	if err != nil {
+		return nil, err
+	}
+	if dbUser == nil {
+		return nil, nil
+	}
+
+	return &model.User{
+		Name:       dbUser.Name,
+		Email:      dbUser.Email,
+		RoleID:     dbUser.RoleID,
+		RoleName:   dbUser.RoleName,
+		LastAccess: dbUser.LastAccess.Time.String(),
+	}, nil
 }
 
-// Login implements UserUseCase.
 func (u *userUseCaseImpl) Login(email string, password string) (*model.User, error) {
-	panic("unimplemented")
+	dbUser, err := u.repo.GetByEmail(email)
+	if err != nil {
+		return nil, err
+	}
+	if dbUser == nil {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	if dbUser.Password != password {
+		return nil, fmt.Errorf("invalid password")
+	}
+
+	return &model.User{
+		Name:       dbUser.Name,
+		Email:      dbUser.Email,
+		RoleID:     dbUser.RoleID,
+		RoleName:   dbUser.RoleName,
+		LastAccess: dbUser.LastAccess.Time.String(),
+	}, nil
 }
 
-// UpdateUser implements UserUseCase.
 func (u *userUseCaseImpl) UpdateUser(user model.User) error {
-	panic("unimplemented")
+	userID, _ := strconv.Atoi(user.ID)
+	dbUser := model.TUser{
+		ID:         userID,
+		Name:       user.Name,
+		Email:      user.Email,
+		Password:   user.Password,
+		RoleID:     user.RoleID,
+		RoleName:   user.RoleName,
+		LastAccess: sql.NullTime{Time: time.Now(), Valid: true},
+	}
+	return u.repo.Update(dbUser)
 }
